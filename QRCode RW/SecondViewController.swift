688e198f956c45b744b5070930e826265af58a2a
Takeo Asai <spitson+github@gmail.com>
2017-07-14 07:43:13 +0900

    First commit

---
+++ QRCode RW/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  QRCode RW
+//
+//  Created by 浅井岳大 on 2017/07/14.
+//  Copyright © 2017年 浅井 岳大. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
